e2e3c3375e8c525586fc83e9a792f79b5aab0224
chidera Ikechukwu <bestbrain407@gmail.com>
2025-09-14 09:57:34 +0000

    added some changes

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -150,16 +150,14 @@ console.log(executeWork(createEmployee(1000)));
 
 
 type Subjects = "Math" | "History";
-
-
 function teachClass(todayClass: Subjects): string {
   if (todayClass === "Math") {
     return "Teaching Math";
+  } else if (todayClass === "History") {
+    return "Teaching History";
   }
-  return "Teaching History";
+
 }
-
-
 console.log(teachClass("Math"));    
 console.log(teachClass("History")); 
 
