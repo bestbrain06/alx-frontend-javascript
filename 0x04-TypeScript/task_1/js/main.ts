045863381ede0d39024c7d1b5fe000b292f3ce1d
chidera Ikechukwu <bestbrain407@gmail.com>
2025-09-14 09:30:02 +0000

    made some changes

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -20,15 +20,14 @@ const director1: Director = {
 console.log(director1);
 
 
-
 interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
 
-const printTeacher: printTeacherFunction = (firstName, lastName) => {
+function printTeacher({ firstName, lastName }: { firstName: string; lastName: string }): string {
   return `${firstName[0]}. ${lastName}`;
-};
+}
 
 
-console.log(printTeacher("John", "Doe"));
+console.log(printTeacher({ firstName: "John", lastName: "Doe" })); 
