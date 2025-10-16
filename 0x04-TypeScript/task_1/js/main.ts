baf97ec0cf86c7c0418986a9ea9d0de813e21809
Chidera Ikechukwu <136890174+bestbrain06@users.noreply.github.com>
2025-10-16 07:30:49 +0000

    Fix printTeacher function to return full first name

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -34,7 +34,7 @@ interface printTeacherFunction {
   ({ firstName, lastName }: { firstName: string; lastName: string }): string;
 }
 function printTeacher(firstName: string, lastName: string): string {
-  return `${firstName.charAt(0)}. ${lastName}`;
+  return `${firstName}. ${lastName}`
 }
 console.log(printTeacher("John", "Doe"));
 
