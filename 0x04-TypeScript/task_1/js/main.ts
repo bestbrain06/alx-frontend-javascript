f231323bbf14af13256f3cc92370092ef79703f3
Chidera Ikechukwu <136890174+bestbrain06@users.noreply.github.com>
2025-10-16 06:55:50 +0000

    Refactor Teacher interface and add teacher3 instance
    Removed Director interface and related code, added teacher3 object.

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,54 +1,18 @@
 interface Teacher {
-  readonly firstName: string;   
-  readonly lastName: string;    
-  fullTimeEmployee: boolean;    
-  yearsOfExperience?: number;   
-  location: string;            
-  [key: string]: any;      
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
 }
 
-interface Director extends Teacher {
-  numberOfReports: number;
-}
-const director1: Director = {
-  firstName: 'John',
-  lastName: 'Doe',
-  location: 'London',
-  fullTimeEmployee: true,
-  numberOfReports: 17,
+const teacher3: Teacher = {
+  firstName: "John",
+  fullTimeEmployee: false,
+  lastName: "Doe",
+  location: "London",
+  contract: false,
 };
-console.log(director1);
 
-
-interface printTeacherFunction {
-  ({ firstName, lastName }: { firstName: string; lastName: string }): string;
-}
-
-function printTeacher({ firstName, lastName }: { firstName: string; lastName: string }): string {
-  return `${firstName}. ${lastName}`;
-}
-
-console.log(printTeacher({ firstName: "John", lastName: "Doe" })); 
-
-class StudentClass {
-  firstName: string;
-  lastName: string;
-
-  constructor(firstName: string, lastName: string) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
-
-  workOnHomework(): string {
-    return "Currently working";
-  }
-
-  displayName(): string {
-    return this.firstName;
-  }
-}
-
-
-const student = new StudentClass("John", "Doe");
-console.log(student.displayName());      
-console.log(student.workOnHomework());   
+console.log(teacher3);
